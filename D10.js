5e8bd62d83c6b3c575b752aa88eccde74ff88e17
Dade1991 <dade91@msn.com>
2025-06-13 15:17:17 +0200

    Complete 1/3

--- D10.js
+++ D10.js
@@ -9,68 +9,171 @@ REGOLE
 
 // JS Basics
 
-/* ESERCIZIO A
+/* ESERCIZIO A - ok
   Crea una variabile chiamata "sum" e assegnaci il risultato della somma tra i valori 10 e 20.
 */
 
-/* ESERCIZIO B
+const sum = 10 + 20
+
+console.log(`Exercise A`, sum)
+
+/* ESERCIZIO B - ok
   Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
 */
 
-/* ESERCIZIO C
+const random = Math.floor(Math.random() * 21)
+
+console.log(`Exercise B`, random)
+
+/* ESERCIZIO C - ok
   Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
 */
 
-/* ESERCIZIO D
+const me = {
+  name: `Davide`,
+  surname: `Braghi`,
+  age: 32,
+}
+
+console.log(`Exercise C`, me)
+
+/* ESERCIZIO D - ok
   Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
 */
 
-/* ESERCIZIO E
+delete me.age
+
+console.log(`Exercise D`, me)
+
+/* ESERCIZIO E - ok
   Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
 */
 
-/* ESERCIZIO F
+me.skills = [`HTML`, `CSS`, `JS (Very bad..!)`, `Other language`]
+
+console.log(`Exercise E`, me)
+
+/* ESERCIZIO F - ok
   Crea un pezzo di codice per aggiungere un nuovo elemento all'array "skills" contenuto nell'oggetto "me".
 */
 
-/* ESERCIZIO G
+const newSkill = `Before learing new code languages, I would like to improve JS!! XD`
+
+me.skills.splice(2, 0, newSkill)
+
+console.log(`Exercise F`, me)
+
+/* ESERCIZIO G - ok
   Crea un pezzo di codice per rimuovere programmaticamente l'ultimo elemento dall'array "skills" contenuto nell'oggetto "me".
 */
 
+delete me.skills[4]
+
+console.log(`Exercise G`, me.skills[4], `Correct because it has been deleted`) // non capisco perchè mi segni undefined
+
 // Funzioni
 
-/* ESERCIZIO 1
+/* ESERCIZIO 1 - ok
   Crea una funzione chiamata "dice": deve generare un numero casuale tra 1 e 6.
 */
 
-/* ESERCIZIO 2
+const dice = () => {
+  const diceResult = Math.ceil(Math.random() * 6)
+  return diceResult
+}
+
+console.log(`Exercise 1`, dice())
+
+dice()
+
+/* ESERCIZIO 2 - ok
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
 */
 
-/* ESERCIZIO 3
+const n1 = 9
+const n2 = 13
+
+const whoIsBigger = () => {
+  if (n1 > n2) {
+    return n1
+  } else {
+    return n2
+  }
+}
+
+console.log(`Exercise 2`, whoIsBigger())
+
+/* ESERCIZIO 3 - ok
   Crea una funzione chiamata "splitMe" che riceve una stringa come parametro e ritorna un'array contenente ogni parola della stringa.
 
   Es.: splitMe("I love coding") => ritorna ["I", "Love", "Coding"]
 */
 
-/* ESERCIZIO 4
+const splitMe = (str1) => {
+  const splitResult = str1.split(" ")
+  console.log(`Exercise 3`, splitResult)
+}
+
+splitMe("I love coding")
+
+/* ESERCIZIO 4 - ok
   Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
   Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
 */
 
-/* ESERCIZIO 5
+const deleteOne = (str2, isTrue) => {
+  if (isTrue) {
+    const resultStr1 = str2.slice(1)
+    console.log(`Exercise 4`, resultStr1)
+  } else {
+    const resultStr2 = str2.slice(0, -1)
+    console.log(`Exercise 4`, resultStr2)
+  }
+}
+
+deleteOne(`CIAO`, false)
+
+/* ESERCIZIO 5 - ok
   Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
 
   Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
 */
 
-/* ESERCIZIO 6
+const onlyLetters = (str3) => {
+  const removeNumber = str3.replace(/[0-9]/g, ``) // ho cercato su internet: la formula applicata in parentesi /[0-9]/g serve per cercare ed escludere i numeri in una stringa. è una regola fissa da utilizzare ogni qualvolta ? è corretto
+  console.log(`Exercise 5`, removeNumber)
+}
+
+onlyLetters(`I have 4 dogs`)
+
+/* ESERCIZIO 6 - ok
   Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
 */
 
-/* ESERCIZIO 7
+const email = `dade91@msn.com`
+
+const isThisAnEmail = (email) => {
+  if (email.includes(`@` && `.com`)) {
+    const emailResult = true
+    console.log(`Exercise 6`, emailResult, `email is valid!`)
+  } else {
+    alert(`Insert a valid email`)
+  }
+}
+
+isThisAnEmail(`dade91@msn.com`)
+
+/* ESERCIZIO 7 - ok
   Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
 */
+
+const now = new Date()
+
+const whatDayIsIt = () => {
+  const today = now.getDate()
+  console.log(`Exercise 7`, today)
+}
+whatDayIsIt()
 
 /* ESERCIZIO 8
   Scrivi una funzione chiamata "rollTheDices" che riceve un numero come parametro.
@@ -85,13 +188,64 @@ REGOLE
   }
 */
 
-/* ESERCIZIO 9
+const rollTheDices = (nDice) => {
+  let sumTot = dice() * nDice
+
+  // console.log(sumTot)
+  // let sum = 0
+  // // console.log(startSum)
+  // for (let i = 0; i < nDice; i++) console.log(startSum)
+  // sum = sum + dice()
+
+  // const sumArray = []
+  //
+  console.log(`Exercise 8 (non completato)`, sumTot)
+}
+rollTheDices(2)
+
+/* ESERCIZIO 9 - ok
   Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
 */
 
-/* ESERCIZIO 10
+const howManyDays = (givenDate, givenMonth, givenYear) => {
+  const now = new Date()
+  const todayDay = now.getDate()
+  const todayMonth = now.getMonth() + 1
+  const todayYear = now.getFullYear()
+  const dayDifference = todayDay - givenDate
+  const monthDifference = todayMonth - givenMonth
+  const yearDifference = todayYear - givenYear
+
+  const fullDifferenceDate = `${dayDifference} ${monthDifference} ${yearDifference}`
+  console.log(
+    `Exercise 9`,
+    ` TIME DIFFERENCES COMPARED TO ACTUAL DATE > days: ${dayDifference}`,
+    `months: ${monthDifference}`,
+    `years: ${yearDifference}`
+  )
+}
+
+howManyDays(7, 6, 2025)
+
+/* ESERCIZIO 10 - ok
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+
+const isTodayMyBirthday = (birthdayDay, birthdayMonth) => {
+  const now = new Date()
+  const todayDay = now.getDate()
+  const todayMonth = now.getMonth() + 1
+
+  const dayDifference = todayDay - birthdayDay
+  const monthDifference = todayMonth - birthdayMonth
+  if (todayDay === birthdayDay && todayMonth === birthdayMonth) {
+    return console.log(`true`)
+  } else {
+    return console.log(`false`)
+  }
+}
+
+isTodayMyBirthday(13, 12)
 
 // Arrays & Oggetti
 
@@ -199,116 +353,116 @@ REGOLE
 
 const movies = [
   {
-    Title: 'The Lord of the Rings: The Fellowship of the Ring',
-    Year: '2001',
-    imdbID: 'tt0120737',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    imdbID: "tt0120737",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
 
   {
-    Title: 'The Lord of the Rings: The Return of the King',
-    Year: '2003',
-    imdbID: 'tt0167260',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    imdbID: "tt0167260",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lord of the Rings: The Two Towers',
-    Year: '2002',
-    imdbID: 'tt0167261',
-    Type: 'movie',
+    Title: "The Lord of the Rings: The Two Towers",
+    Year: "2002",
+    imdbID: "tt0167261",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of War',
-    Year: '2005',
-    imdbID: 'tt0399295',
-    Type: 'movie',
+    Title: "Lord of War",
+    Year: "2005",
+    imdbID: "tt0399295",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
   },
   {
-    Title: 'Lords of Dogtown',
-    Year: '2005',
-    imdbID: 'tt0355702',
-    Type: 'movie',
+    Title: "Lords of Dogtown",
+    Year: "2005",
+    imdbID: "tt0355702",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lord of the Rings',
-    Year: '1978',
-    imdbID: 'tt0077869',
-    Type: 'movie',
+    Title: "The Lord of the Rings",
+    Year: "1978",
+    imdbID: "tt0077869",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of the Flies',
-    Year: '1990',
-    imdbID: 'tt0100054',
-    Type: 'movie',
+    Title: "Lord of the Flies",
+    Year: "1990",
+    imdbID: "tt0100054",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
   },
   {
-    Title: 'The Lords of Salem',
-    Year: '2012',
-    imdbID: 'tt1731697',
-    Type: 'movie',
+    Title: "The Lords of Salem",
+    Year: "2012",
+    imdbID: "tt1731697",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
   },
   {
-    Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
-    Year: '1984',
-    imdbID: 'tt0087365',
-    Type: 'movie',
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+    Year: "1984",
+    imdbID: "tt0087365",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
   },
   {
-    Title: 'Lord of the Flies',
-    Year: '1963',
-    imdbID: 'tt0057261',
-    Type: 'movie',
+    Title: "Lord of the Flies",
+    Year: "1963",
+    imdbID: "tt0057261",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
   },
   {
-    Title: 'The Avengers',
-    Year: '2012',
-    imdbID: 'tt0848228',
-    Type: 'movie',
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Infinity War',
-    Year: '2018',
-    imdbID: 'tt4154756',
-    Type: 'movie',
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Age of Ultron',
-    Year: '2015',
-    imdbID: 'tt2395427',
-    Type: 'movie',
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
   },
   {
-    Title: 'Avengers: Endgame',
-    Year: '2019',
-    imdbID: 'tt4154796',
-    Type: 'movie',
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
 ]
